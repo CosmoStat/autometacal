98bb8e19c6833775d8cfe8cdc39c5afd84c79b72
EiffL <fr.eiffel@gmail.com>
2021-04-13 14:30:00 +0200

    Adds first draft of metacal image generation

--- autometacal/python/metacal.py
+++ autometacal/python/metacal.py
@@ -5,4 +5,25 @@ import tensorflow as tf
 def generate_mcal_images(galaxy_stamps, noise_stamps, PSF, g):
   """ Generate a metacalibrated image.
   """
-  pass
+  # Step1: remove observed psf
+  img = galaxy_stamps * inv_psf_img
+  imgn = noise_stamps * inv_psf_img
+
+  # Step2: add shear layer
+  img = gf.shear(tf.expand_dims(img,-1), g1, g2)[...,0]
+  imgn = gf.shear(tf.expand_dims(imgn,-1), -g1, -g2)[...,0]
+
+  # Step3: apply psf again
+  img = gf.kconvolve(to_rfft(img), (psf_img))[...,0]
+  img = tf.expand_dims(tf.signal.fftshift(img),-1)
+  img = tf.image.resize_with_crop_or_pad(img, 256, 256)
+
+  imgn = gf.kconvolve(to_rfft(imgn), (psf_img))[...,0]
+  imgn = tf.expand_dims(tf.signal.fftshift(imgn),-1)
+  imgn = tf.image.resize_with_crop_or_pad(imgn, 256, 256)
+
+  # Adding the inversed sheared noise
+  img += imgn
+
+  # Step4: compute ellipticity
+  return img, tf.stack(get_ellipticity(img[0,:,:,0] ))
