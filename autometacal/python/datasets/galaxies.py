cd8d2d9a8441cabb0d9b7f4862290b3748b9239a
André Z. Vitorelli <andrezvitorelli@gmail.com>
2021-10-27 19:16:03 +0200

    work on datasets

--- autometacal/python/datasets/galaxies.py
+++ autometacal/python/datasets/galaxies.py
@@ -44,3 +44,37 @@ def make_data(Ngals=1,
   
   return gal_image_stack, psf_image_stack
 
+
+def make_parametric_cosmos(ccat,j, **kwargs):
+  """Creates CFIS-like images out of the parametric COSMOS galaxies."""
+  #CFIS
+  {'psf_fwhm' : .65, # arcsec
+   'sky_level': 400, # ADU (~variance)
+  'pixel_scale' : 0.187, # arcsec/pixel
+  'mag_zp' : 32,
+  'stamp_size' : 51}
+  
+  defaults.update(kwargs)
+ 
+  #get magnitude & flux
+  gal_mag = ccat.param_cat[j][1]
+  gal_flux = 10**(-(gal_mag-mag_zp)/2.5)
+
+  psf = galsim.Kolmogorov(fwhm=psf_fwhm).withFlux(1.0)
+  
+  gal = ccat.makeGalaxy(index=j,gal_type='parametric').withFlux(gal_flux)
+
+  obj = galsim.Convolve((psf, gal))
+  img = obj.drawImage(nx=stamp_size, ny=stamp_size, scale=pixel_scale, method='fft')
+
+  # Get snr
+  S2 = np.sum(img.array**2)
+  s2n = np.sqrt(S2/sky_level)
+
+  noise = galsim.GaussianNoise(sigma=np.sqrt(sky_level), rng=gal_rng)
+  img.addNoise(noise) # it updates directly the galsim object. It returns
+                      # the variance if I remember well..
+
+  #print(f"s2n: {s2n} (in galsim system)")
+  return img.array, s2n, gal_mag
+
