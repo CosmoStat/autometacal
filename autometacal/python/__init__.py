b9e524d6aef98c7cbe09dee16d2156a741a03d5f
André Z. Vitorelli <andrezvitorelli@gmail.com>
2022-04-06 00:36:47 +0200

    creating code to assess the PSF response

--- autometacal/python/__init__.py
+++ autometacal/python/__init__.py
@@ -1,5 +1,6 @@
 import autometacal.python.datasets as datasets
 import autometacal.python.tf_ngmix as tf_ngmix
 from autometacal.python.metacal import generate_mcal_image, get_metacal_response, get_metacal_response_finitediff
+from autometacal.python.metacal_psf import generate_psf_mcal_image, get_psf_metacal_response
 from autometacal.python.fitting import fit_multivariate_gaussian, get_ellipticity
 from autometacal.python.moments import get_moment_ellipticities, gaussian_moments
