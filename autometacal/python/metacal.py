e79749eca39b24f8a088a511003081fd121dee40
André Z. Vitorelli <andrezvitorelli@gmail.com>
2022-05-09 11:13:01 +0200

    adding function to apply shears before psfs or noise

--- autometacal/python/metacal.py
+++ autometacal/python/metacal.py
@@ -196,3 +196,84 @@ def get_metacal_response_finitediff(gal_image,
   } 
 
   return ellip_dict, R
+
+def generate_real_mcal_image(
+  gal_images,
+  reconvolution_psf_image,
+  g
+):
+  """ Generate artificial sheared image sheared before PSF.
+  
+  Args: 
+    gal_images: tf.Tensor or np.array
+      (batch_size, N, N ) image of galaxies
+    reconvolution_psf_image: tf.Tensor
+      (N, N ) tensor of reconvolution psf model
+    g: tf.Tensor or np.array
+    [batch_size, 2] input shear
+  Returns:
+    img: tf.Tensor
+      tf tensor containing image of galaxy after deconvolution by psf_deconv, 
+      shearing by g, and reconvolution with reconvolution_psf_image.
+  
+  """
+  #cast stuff as float32 tensors
+  gal_images = tf.convert_to_tensor(gal_images, dtype=tf.float32)  
+  #psf_images = tf.convert_to_tensor(psf_images, dtype=tf.float32) 
+  reconvolution_psf_image = tf.convert_to_tensor(reconvolution_psf_image, dtype=tf.float32)  
+  g = tf.convert_to_tensor(g, dtype=tf.float32)  
+  
+  #Get batch info
+  batch_size, nx, ny = gal_images.get_shape().as_list()  
+      
+  #add pads in real space
+  padfactor = 3 #total width of image after padding
+  fact = (padfactor - 1)//2 #how many image sizes to one direction
+  paddings = tf.constant([[0, 0,], [nx*fact, nx*fact], [ny*fact, ny*fact]])
+    
+  padded_gal_images = tf.pad(gal_images,paddings)
+  padded_psf_images = tf.pad(psf_images,paddings)
+  padded_reconvolution_psf_image = tf.pad(reconvolution_psf_image,paddings)
+    
+  #Convert galaxy images to k space
+  im_shift = tf.signal.ifftshift(padded_gal_images,axes=[1,2]) # The ifftshift is to remove the phase for centered objects
+  im_complex = tf.cast(im_shift, tf.complex64)
+  im_fft = tf.signal.fft2d(im_complex)
+  imk = tf.signal.fftshift(im_fft, axes=[1,2])#the fftshift is to put the 0 frequency at the center of the k image
+  
+  #Convert psf images to k space  
+  #psf_complex = tf.cast(padded_psf_images, tf.complex64)
+  #psf_fft = tf.signal.fft2d(psf_complex)
+  #psf_fft_abs = tf.abs(psf_fft)
+  #psf_fft_abs_complex = tf.cast(psf_fft_abs,tf.complex64)
+  #kpsf = tf.signal.fftshift(psf_fft_abs_complex,axes=[1,2])
+
+  #Convert reconvolution psf image to k space 
+  rpsf_complex = tf.cast(padded_reconvolution_psf_image, tf.complex64)
+  rpsf_fft =  tf.signal.fft2d(rpsf_complex)
+  rpsf_fft_abs = tf.abs(rpsf_fft)
+  psf_fft_abs_complex = tf.cast(rpsf_fft_abs,tf.complex64)
+  krpsf = tf.signal.fftshift(psf_fft_abs_complex,axes=[1,2])
+
+  # Compute Fourier mask for high frequencies
+  # careful, this is not exactly the correct formula for fftfreq
+  kx, ky = tf.meshgrid(tf.linspace(-0.5,0.5,padfactor*nx),
+                       tf.linspace(-0.5,0.5,padfactor*ny))
+  mask = tf.cast(tf.math.sqrt(kx**2 + ky**2) <= 0.5, dtype='complex64')
+  mask = tf.expand_dims(mask, axis=0)
+
+  # Don't Deconvolve image from input PSF
+  im_deconv = imk * mask #* ( (1./(kpsf+1e-10))*mask)
+
+  # Apply shear
+  im_sheared = gf.shear(tf.expand_dims(im_deconv,-1), g[...,0], g[...,1])[...,0]
+
+  # Reconvolve with target PSF
+  im_reconv = tf.signal.ifft2d(tf.signal.ifftshift(im_sheared * krpsf * mask))
+
+  # Compute inverse Fourier transform
+  img = tf.math.real(tf.signal.fftshift(im_reconv))
+  
+  img = img[:,fact*nx:-fact*nx,fact*ny:-fact*ny]
+
+  return img
