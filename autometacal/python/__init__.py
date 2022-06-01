eaa0e9a141591bfe54c3dac14144eb85355117ec
Benjamin Remy <remy.benjamin.101@gmail.com>
2022-06-01 19:06:19 +0200

    first try selection R

--- autometacal/python/__init__.py
+++ autometacal/python/__init__.py
@@ -1,7 +1,7 @@
-from autometacal.python import datasets 
-import autometacal.python.tf_ngmix as tf_ngmix
-from autometacal.python.metacal import generate_mcal_image, get_metacal_response, get_metacal_response_finitediff
-from autometacal.python.metacal_psf import generate_psf_mcal_image, get_psf_metacal_response
-from autometacal.python.fitting import fit_multivariate_gaussian, get_ellipticity
-from autometacal.python.moments import get_moment_ellipticities, gaussian_moments
-from autometacal.python.utils import *
+#from autometacal.python import datasets 
+#import autometacal.python.tf_ngmix as tf_ngmix
+#from autometacal.python.metacal import generate_mcal_image, get_metacal_response, get_metacal_response_finitediff
+#from autometacal.python.metacal_psf import generate_psf_mcal_image, get_psf_metacal_response
+#from autometacal.python.fitting import fit_multivariate_gaussian, get_ellipticity
+#from autometacal.python.moments import get_moment_ellipticities, gaussian_moments
+#from autometacal.python.utils import *
