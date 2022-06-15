a0f62da3e65246eac4f59bfdf3cedcbef34e6e54
André Z. Vitorelli <andrezvitorelli@gmail.com>
2022-06-15 17:29:53 +0200

    additional modifications to the datasets to ease change when testing

--- autometacal/python/__init__.py
+++ autometacal/python/__init__.py
@@ -1,5 +1,7 @@
-from autometacal.python import datasets 
+import autometacal.python.datasets as datasets 
 import autometacal.python.tf_ngmix as tf_ngmix
+import autometacal.python.metacal_psf as metacal_psf
 from autometacal.python.metacal import generate_mcal_image, get_metacal_response, get_metacal_response_finitediff
 from autometacal.python.fitting import fit_multivariate_gaussian, get_ellipticity
 from autometacal.python.moments import get_moment_ellipticities, gaussian_moments
+from autometacal.python.utils import *
